01a3772728ad42e096b268fe92a75ed756acea30
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-03-17 00:01:28 +0100

    test: add test check credit card network

--- src/creditCardValidator.spec.ts
+++ src/creditCardValidator.spec.ts
@@ -43,7 +43,27 @@ describe('Given a credit card validator', () => {
         isValid: false,
         errors: ['The card is not valid according to the Luhn algorithm'],
       };
-      const cardNumber = '1234567812345678';
+      const cardNumber = '2214567812345678';
+      const expirationDate = '10/24';
+
+      const cardResponse = creditCardValidator({
+        cardNumber,
+        expirationDate,
+      });
+
+      expect(cardResponse).toEqual(expectedCardResponse);
+    });
+  });
+
+  describe('When the card numbre is not in a valid network', () => {
+    it('Then the function should return a non valid card object and the network error', () => {
+      const expectedCardResponse = {
+        isValid: false,
+        errors: [
+          'The card must be from one of the following networks: Visa, Mastercard, American Express or Diners Club',
+        ],
+      };
+      const cardNumber = '7001111111111117';
       const expirationDate = '10/24';
 
       const cardResponse = creditCardValidator({
