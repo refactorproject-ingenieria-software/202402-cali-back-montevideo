39a155dcf5e1bb28c3d47519d24f7e11de1d2d2e
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-03-16 10:52:31 +0100

    feat: add arguments verification at creditCardValidator

--- src/creditCardValidator.spec.ts
+++ src/creditCardValidator.spec.ts
@@ -1,7 +1,14 @@
+const creditCardValidator = (cardNumber: string, expirationDate: string) => {
+  if (typeof cardNumber !== 'string' || typeof expirationDate !== 'string') {
+    throw 'Parameters should be string';
+  }
+};
 describe('Given a credit card validor', () => {
   describe('When the functions receives a non string input', () => {
     it('Then the function should raise an error', () => {
-      expect(() => {}).toThrow('Parameters should be string');
+      expect(() =>
+        creditCardValidator('string', 5 as unknown as string),
+      ).toThrow('Parameters should be string');
     });
   });
 });
