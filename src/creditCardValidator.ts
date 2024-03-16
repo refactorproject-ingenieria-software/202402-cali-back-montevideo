eb654c1f8cbdb33de341cf5c0e44344497887820
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-03-17 00:14:07 +0100

    refactor: separate logic inside luhn algorithm

--- src/creditCardValidator.ts
+++ src/creditCardValidator.ts
@@ -11,17 +11,20 @@ const errorGenerator = (field: string, type: string): string => {
   return `${field} should be ${type}`;
 };
 
+const sumLuhnNumbers = (i: number, cardNumber: string, checkSum: number) => {
+  const isEven = i % 2 === 0;
+  const currentDigit = cardNumber.charAt(i);
+  let numberDigit = parseInt(currentDigit, 10);
+  if (isEven && (numberDigit *= 2) > 9) numberDigit -= 9;
+  checkSum += numberDigit;
+  return checkSum;
+};
+
 const isValidCardNumberByLuhn = (cardNumber): boolean => {
   let checkSum = 0;
   for (let i = cardNumber.length - 1; i >= 0; i--) {
-    const isEven = i % 2 === 0;
-    const currentDigit = cardNumber.charAt(i);
-    let numberDigit = parseInt(currentDigit, 10);
-    if (isEven && (numberDigit *= 2) > 9) numberDigit -= 9;
-
-    checkSum += numberDigit;
+    checkSum = sumLuhnNumbers(i, cardNumber, checkSum);
   }
-
   return checkSum % 10 == 0;
 };
 
