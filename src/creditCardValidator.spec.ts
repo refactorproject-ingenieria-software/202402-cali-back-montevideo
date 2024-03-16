1d9c23fc13284170f947de24cb29762c460644d0
Cristian <cristian@Cristians-MacBook-Pro.local>
2024-03-16 11:16:27 +0100

    refactor: changes test logic for type guard

--- src/creditCardValidator.spec.ts
+++ src/creditCardValidator.spec.ts
@@ -2,10 +2,23 @@ import { creditCardValidator } from './creditCardValidator';
 
 describe('Given a credit card validor', () => {
   describe('When the functions receives a non string input', () => {
-    it('Then the function should raise an error', () => {
-      expect(() =>
-        creditCardValidator('string', 5 as unknown as string),
-      ).toThrow('Parameters should be string');
+    it('Then the function should return a non valid card object and the validation error', () => {
+      const expectedCardResponse = {
+        valid: false,
+        errors: [
+          'cardNumber should be string',
+          'expirationDate should be string',
+        ],
+      };
+      const cardNumber = 'string';
+      const wrongTypeexpirationDate = 5;
+
+      const cardResponse = creditCardValidator({
+        cardNumber,
+        expirationDate: wrongTypeexpirationDate,
+      });
+
+      expect(cardResponse).toEqual(expectedCardResponse);
     });
   });
 });
