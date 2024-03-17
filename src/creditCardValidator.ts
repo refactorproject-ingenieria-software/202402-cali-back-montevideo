da791711db6d808a97d161fa6887338847728c89
Cristian <cristian@Cristians-MacBook-Pro.local>
2024-03-17 12:44:53 +0100

    refactor: Updates creditCardValidator logic from codeRabbit reviews

--- src/creditCardValidator.ts
+++ src/creditCardValidator.ts
@@ -7,44 +7,75 @@ type CardValidatorResponse = {
   errors: string[];
 };
 
-const errorGenerator = (field: string, type: string): string => {
+// Generates error messages for invalid fields type
+const fieldTypeErrorMessageGenerator = (
+  field: string,
+  type: string,
+): string => {
   return `${field} should be ${type}`;
 };
 
-const sumLuhnNumbers = (i: number, cardNumber: string, checkSum: number) => {
-  const isEven = i % 2 === 0;
-  const currentDigit = cardNumber.charAt(i);
+const currentDigitValueByLuhn = (currentDigit, isEven) => {
   let numberDigit = parseInt(currentDigit, 10);
   if (isEven && (numberDigit *= 2) > 9) numberDigit -= 9;
-  checkSum += numberDigit;
-  return checkSum;
+  return numberDigit;
 };
 
-const isValidCardNumberByLuhn = (cardNumber): boolean => {
-  let checkSum = 0;
+/**
+ * Sums digits according to the Luhn algorithm
+ *
+ * @param {number} i - Current index in the card number
+ * @param {string} cardNumber - The card number being validated
+ * @param {number} cardNumberSum - The accumulated sum value until last index
+ * @return {number} The accumulated sum value for current index
+ */
+const sumCardNumberByLuhn = (
+  i: number,
+  cardNumber: string,
+  cardNumberSum: number,
+) => {
+  const isEven = i % 2 === 0;
+  const currentDigit = cardNumber.charAt(i);
+  const currentDigitLuhnValue = currentDigitValueByLuhn(currentDigit, isEven);
+  cardNumberSum += currentDigitLuhnValue;
+  return cardNumberSum;
+};
+
+const checkSumLuhnConditions = (sumAmount) => {
+  const isMorenThanZero = sumAmount > 0;
+  const isTeenFactor = sumAmount % 10 === 0;
+  return isMorenThanZero && isTeenFactor;
+};
+
+const isValidCardNumberByLuhn = (cardNumber: string): boolean => {
+  let cardNumberSum = 0;
   for (let i = cardNumber.length - 1; i >= 0; i--) {
-    checkSum = sumLuhnNumbers(i, cardNumber, checkSum);
+    cardNumberSum = sumCardNumberByLuhn(i, cardNumber, cardNumberSum);
   }
-  return checkSum % 10 == 0;
+  return checkSumLuhnConditions(cardNumberSum);
 };
 
-const creditCardTypeGuards = (
+const validateCreditCardFieldsType = (
   response: CardValidatorResponse,
   cardNumber: string,
   expirationDate: string,
 ): void => {
   if (typeof cardNumber !== 'string') {
     response.isValid = false;
-    response.errors.push(errorGenerator('cardNumber', 'string'));
+    response.errors.push(
+      fieldTypeErrorMessageGenerator('cardNumber', 'string'),
+    );
   }
 
   if (typeof expirationDate !== 'string') {
     response.isValid = false;
-    response.errors.push(errorGenerator('expirationDate', 'string'));
+    response.errors.push(
+      fieldTypeErrorMessageGenerator('expirationDate', 'string'),
+    );
   }
 };
 
-const creditCardCardNumberGuards = (
+const validateCreditCardCardNumber = (
   response: CardValidatorResponse,
   cardNumber: string,
 ): void => {
@@ -61,7 +92,7 @@ const creditCardCardNumberGuards = (
   }
 };
 
-const creditCardNetworkGuards = (
+const validateCreditCardNetwork = (
   response: CardValidatorResponse,
   cardNumber: string,
 ): void => {
@@ -81,16 +112,16 @@ const creditCardNetworkGuards = (
   }
 };
 
-const verifyFormatExpirationDate = (expirationDate: string): boolean => {
+const isValidExpirationDateFormat = (expirationDate: string): boolean => {
   const regex = /^(0[1-9]|1[0-2])\/([2-9][0-9])$/;
-  return !regex.test(expirationDate);
+  return regex.test(expirationDate);
 };
 
-const creditCardExpirationDateGuard = (
+const validateCreditCardExpirationDate = (
   response: CardValidatorResponse,
   expirationDate: string,
 ): void => {
-  if (verifyFormatExpirationDate(expirationDate)) {
+  if (!isValidExpirationDateFormat(expirationDate)) {
     (response.isValid = false),
       response.errors.push('The card must have a valid expiration date');
   }
@@ -105,10 +136,10 @@ export const creditCardValidator = ({
     errors: [],
   };
 
-  creditCardTypeGuards(response, cardNumber, expirationDate);
-  creditCardCardNumberGuards(response, cardNumber);
-  creditCardNetworkGuards(response, cardNumber);
-  creditCardExpirationDateGuard(response, expirationDate);
+  validateCreditCardFieldsType(response, cardNumber, expirationDate);
+  validateCreditCardCardNumber(response, cardNumber);
+  validateCreditCardNetwork(response, cardNumber);
+  validateCreditCardExpirationDate(response, expirationDate);
 
   return response;
 };
